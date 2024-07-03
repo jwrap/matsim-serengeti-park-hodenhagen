2e2d0a0ffdbc18d1e87f688331ccca501d59539a
jwrap <jryap9@gmail.com>
2024-07-03 22:33:39 +0800

    second simple event handler

--- src/main/java/org/matsim/analysis/SimplePersonEventHandler.java
+++ src/main/java/org/matsim/analysis/SimplePersonEventHandler.java
@@ -1,18 +1,33 @@
 package org.matsim.analysis;
 
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.events.PersonArrivalEvent;
 import org.matsim.api.core.v01.events.PersonDepartureEvent;
 import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
+import org.matsim.api.core.v01.population.Person;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SimplePersonEventHandler implements PersonDepartureEventHandler, PersonArrivalEventHandler {
-    @Override
-    public void handleEvent(PersonArrivalEvent personArrivalEvent) {
-        System.out.println("Arrival: " + personArrivalEvent.getTime() + ": " + personArrivalEvent.getPersonId());
-    }
-
+    private Map<Id<Person>, Double> personToDepartureTime = new HashMap<>();
     @Override
     public void handleEvent(PersonDepartureEvent personDepartureEvent) {
-        System.out.println("Departure: " + personDepartureEvent.getTime() + ": " + personDepartureEvent.getPersonId());
+        var departureTime = personDepartureEvent.getTime();
+        var personId = personDepartureEvent.getPersonId();
+
+        personToDepartureTime.put(personId, departureTime);
+        // System.out.println("Departure: " + personDepartureEvent.getTime() + ": " + personDepartureEvent.getPersonId());
     }
+    @Override
+    public void handleEvent(PersonArrivalEvent personArrivalEvent) {
+        var arrivalTime = personArrivalEvent.getTime();
+        var departureTime = personToDepartureTime.get(personArrivalEvent.getPersonId());
+        var travelTime = arrivalTime - departureTime;
+//        System.out.println("Arrival: " + personArrivalEvent.getTime() + ": " + personArrivalEvent.getPersonId());
+        System.out.println("Person: " + personArrivalEvent.getPersonId() + " travelled: " + travelTime + "s.");
+    }
+
+
 }
